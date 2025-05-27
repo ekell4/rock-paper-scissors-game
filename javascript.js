94e8f3a7f37a1859dba042beada4bf2640542497
ekell4 <createdbyeryn@gmail.com>
2025-05-27 13:02:10 -0400

    Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
-console.log("Hello World!")
+//When page is loaded
+//Randomly generate a decimal value between 0 and 1
+//If value is less than 0.3
+//Return rock
+//If value is greater than 0.3 but less than 0.6
+//Return paper
+//If value is greater than 0.6
+//Return scissor
+
+function getComputerChoice() {
+    let a = Math.random();
+    let result;
+    if (a<=0.3) {
+        result = "rock";
+    } else if (a>0.3 && a<=0.6) {
+        result = "paper";
+    } else if (a>0.6) {
+        result = "scissor";
+    }
+        return result;
+    }
+    
+
+getComputerChoice();
