9c595f2822fba278bcf629e1d4e155154278449f
ekell4 <createdbyeryn@gmail.com>
2025-05-27 13:22:06 -0400

    Add function getHumanChoice

--- javascript.js
+++ javascript.js
@@ -22,3 +22,29 @@ function getComputerChoice() {
     
 
 getComputerChoice();
+
+//When page is loaded
+//Open a prompt window asking "Rock, paper or scissors?"
+//When user inputs choice
+//Check choice against computer choice
+//If user and computer make the same choice
+//Alert "It's a tie! You made the same choice. Choose again"
+//If user chooses rock, and computer chooses scissors
+//Alert "Yes! Rock crushes scissors."
+//If user chooses rock, and computer chooses paper
+//Alert "No. Paper covers rock."
+//If user chooses paper, and computer chooses rock
+//Alert "Yes! Paper covers rock."
+//If user chooses paper, and computer chooses scissors
+//Alert "No. Scissors cut paper."
+//If user chooses scissors, and computer chooses paper
+//Alert "Yes! Scissors cut paper."
+//If user chooses scissors, and computer chooses rock
+//Alert "No. Rock crushes scissors"
+
+function getHumanChoice() {
+    let choice = prompt("Rock, paper, or scissors?");
+}
+
+getHumanChoice();
+
